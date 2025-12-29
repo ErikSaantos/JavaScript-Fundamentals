c6b5da858eb82f258e2d6d457e7814d0cd02d20b
ErikSantos <erik.silva@picpay.com>
2025-12-29 09:58:41 -0300

    Exercises of destructuring arrays and objects

---
+++ 05_destructuring/app.js
@@ -0,0 +1,47 @@
+// Destructuring allow us to "unpack" values 
+// from data-structures (Arrays, objects) into separate distinct variables.
+
+// const foo = ['elem1', 'elem2', 'elem3']
+
+// const [value1, value2, value3, value4, value5] = foo;
+
+// console.log(value1);
+// console.log(value5);
+
+// // Setting default values
+// let a, b;
+
+// [a = 5, b = 8] = ["new value", "now for b"];
+// console.log(a);
+// console.log(b);
+
+
+// ========== Storing or extracting some data from a function: ==========
+// function f(){
+//     return [3, 8, 23]
+// };
+
+// let el, el2;
+
+// [el, el2] = f()
+
+// console.log(el);
+// console.log(el2);
+
+// // Ignoring some index
+// const [a, , b] = f()
+// console.log(a);
+// console.log(b);
+
+// ========== Object Destructuring ==========
+// In object destructuring order doesn't matter but the name does
+
+const student = {
+    name: "Erik",
+    position: "Firrst",
+    rollNo: 11
+};
+
+const {name, rollNo} = student;
+console.log(name);
+console.log(rollNo);
