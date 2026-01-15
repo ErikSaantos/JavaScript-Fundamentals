728a64c6b83259ca472771bf329f201842e93b0a
ErikSantos <erik.silva@picpay.com>
2026-01-15 08:43:31 -0300

    'Style' exercises

---
+++ DOM/06. Style/app.js
@@ -0,0 +1,5 @@
+const h1 = document.querySelector("h1");
+// console.log(h1.style); // Try dont use this way
+h1.style.color = "teal";
+h1.style.backgroundColor = "black";
+h1.style.textAlign = "center";
